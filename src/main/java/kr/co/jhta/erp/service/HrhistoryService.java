e7f4128160d1a142841dd11bf47f82cd44509070
tpleehan <tpleehan@gmail.com>
2023-03-24 23:49:53 +0900

    :lipstick:style: 중앙HTA Final Project

---
+++ src/main/java/kr/co/jhta/erp/service/HrhistoryService.java
@@ -0,0 +1,12 @@
+package kr.co.jhta.erp.service;
+
+import java.util.Map;
+
+import kr.co.jhta.erp.form.InternalMobilityAddForm;
+
+public interface HrhistoryService {
+
+	void addInternalMobility(InternalMobilityAddForm forms);
+
+	Map<String, Object> searchHrHistories(Map<String, Object> criteria);
+}
